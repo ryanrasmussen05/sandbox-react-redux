1abd7f3c47362c7b2f1e7f76c80e6b869209db6f
Ryan <ryanrasmussen05@gmail.com>
2019-01-09 14:52:32 -0600

    top nav done

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Menu, Button, Dropdown, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 import './Header.scss';
+import {MenuMode} from "antd/lib/menu";
 
 interface HeaderProps {
-    currentPath: string
+    currentPath: string;
 }
+
+interface NavMenuProps {
+    mode: MenuMode;
+    activeLink: string;
+    className: string;
+}
+
+const NavMenu: React.FunctionComponent<NavMenuProps> = (props) => {
+    return (
+        <Menu
+            mode={props ? props.mode : 'vertical'}
+            selectedKeys={props ? [props.activeLink] : []}
+            className={props ? props.className : ''}
+        >
+            <Menu.Item key="/"><Link to="/">Home</Link></Menu.Item>
+            <Menu.Item key="/resume"><Link to="/resume">Resume</Link></Menu.Item>
+            <Menu.Item key="/sandbox"><Link to="/sandbox">Sandbox</Link></Menu.Item>
+        </Menu>
+    )
+};
 
 class Header extends React.Component<HeaderProps> {
     render() {
         return (
             <div className="header flex items-center justify-between">
                 <div className="title">Ryan Rasmussen</div>
-                <Menu
-                    mode="horizontal"
-                    selectedKeys={[this.props.currentPath]}
-                >
-                    <Menu.Item key="/"><Link to="/">Home</Link></Menu.Item>
-                    <Menu.Item key="/resume"><Link to="/resume">Resume</Link></Menu.Item>
-                    <Menu.Item key="/sandbox"><Link to="/sandbox">Sandbox</Link></Menu.Item>
-                </Menu>
+
+                <Dropdown overlay={NavMenu} trigger={['click']} className="nav-dropdown">
+                    <Button htmlType="button" type="primary" icon="bars"></Button>
+                </Dropdown>
+
+                <NavMenu mode="horizontal" activeLink={this.props.currentPath} className="nav-menu"/>
             </div>
         );
     }
