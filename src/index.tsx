820a35540a8853e6b6a635b654f09dbeab93777c
Ryan <ryanrasmussen05@gmail.com>
2019-01-15 16:02:23 -0600

    fix ie 11

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import 'react-app-polyfill/ie11';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
